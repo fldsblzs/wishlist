ebf3bd537d7073ba56d081348fc939a2ced8a0fd
Balázs Földesi <foldesibalazs@gmail.com>
2022-11-14 14:04:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# SvelteKit + Supabase template
+
+Template repository for starting a new SvelteKit projects using Supabase.
+
+- [Svelte](https://svelte.dev/)
+- [SvelteKit](https://kit.svelte.dev/)
+- [Tailwind CSS](https://tailwindcss.com/)
+- [DaisyUI](https://daisyui.com/)
+- [Supabase](https://supabase.com/)
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import '../app.css';
+	import { supabaseClient } from '$lib/db';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabaseClient.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth');
+		});
+
+		return () => {
+			subscription.unsubscribe();
+		};
+	});
+</script>
+
+<slot />
