83b1d31b860f6320cbfe3152713c5e28bafb5375
fldsblzs <foldesibalazs@gmail.com>
2022-11-19 14:59:14 +0100

    Update page svg source

--- src/routes/(app)/wisher/[id]/+page.svelte
+++ src/routes/(app)/wisher/[id]/+page.svelte
@@ -11,6 +11,6 @@
 {:else}
 	<div class="flex flex-col justify-center mt-10 items-center">
 		<h1 class="text-3xl">Oh, no! No <span class="text-primary">wishes</span> yet!</h1>
-		<img class="h-52 my-10" src="empty.svg" alt="Empty" />
+		<img class="h-52 my-10" src="/empty.svg" alt="Empty" />
 	</div>
 {/if}
