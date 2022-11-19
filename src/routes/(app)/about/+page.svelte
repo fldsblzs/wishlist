3522c4b9a1e1349183bada2e33d6e14b9ac01469
fldsblzs <foldesibalazs@gmail.com>
2022-11-19 18:21:49 +0100

    Fix spacing on about page

--- src/routes/(app)/about/+page.svelte
+++ src/routes/(app)/about/+page.svelte
@@ -1,11 +1,11 @@
-<div class="flex flex-col justify-center mt-10 items-center">
+<div class="flex flex-col justify-center mx-10 items-center">
 	<h1 class="text-3xl">About <span class="text-primary">wish</span>list</h1>
 	<img class="h-52 my-10" src="snow_globe.svg" alt="" />
 	<div>🎄🎁🎄</div>
-	<p class="text-lg">
+	<p class="text-lg text-center">
 		Check out each person's own wishlist using the navbar <span class="text-primary">above</span>.
 	</p>
-	<p class="font-thin">
+	<p class="font-thin text-center">
 		Simply select the options you want to chose and that's it! You can also change your mind later!
 	</p>
 	<div>🎁🎄🎁</div>
